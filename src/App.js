f646927ecc362581930347c76db255f3468e5cc3
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-28 10:56:18 +0300

    conditional

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 
 function App() {
   const userIsLoggedIn = true;
+  const age = 30;
   return (
     <div>
       <h2>App</h2>
       <div className='controll'>
-        {userIsLoggedIn === true ? <button>Go to About</button> : ''}
+        {/* {userIsLoggedIn ? <button>Go to About</button> : <button>Login</button>} */}
+        {userIsLoggedIn && <button>Go to About</button>}
+        {!userIsLoggedIn && <button>Login</button>}
       </div>
+      <article>
+        <h2>Your age is: {age}</h2>
+        {age >= 18 && <h3>You are adult</h3>}
+        {age < 18 && <h3>You are not adult</h3>}
+      </article>
+
+      <button className={age >= 18 ? 'btn-adult' : 'btn-teen'}>
+        Age {age >= 18 ? 'adult' : 'not-adult'}
+      </button>
     </div>
   );
 }
