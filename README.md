09c947aa4b834c1c858d23376ff77066000a30a7
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-29 13:09:16 +0300

    uzd practice

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 2. Conditionals
 
 ## REACT 4 State, Hooks, Lifecycle
+
+1. Create components Salary, SalaryList
+2. Salary prints name and amount in eur (200.00 eur)
+3. SalaryList generate Salary components passing name and number
+4. Salary has button to ingrease or decrese salary by 100
+5. Add buttons to salaryList to display 2, 3 or 4 salary elements in a row
+6. Add button to salary that toggle night mode
--- src/components/People/Person.js
+++ src/components/People/Person.js
@@ -1,7 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Person(props) {
   const [clickNr, setClicksNr] = useState(1);
+
+  // this render every time compoement changes
+  document.title = 'Person stuff';
+  console.log('title changed');
+
+  // on component mount change document title to 'Person stuff'
+  useEffect(() => {
+    document.title = 'Person stuff';
+    console.log('title changed');
+  }, []);
 
   const onInrement = () => {
     console.log('clicked');
@@ -26,11 +36,10 @@ export default function Person(props) {
       <h2>{props.children}</h2>
       <p>{clickNr}</p>
       <button onClick={onInrement}>click</button>
-      {clickNr > 0 && (
-        <button disabled onClick={onDecrement}>
-          - 1
-        </button>
-      )}
+
+      <button disabled={clickNr === 0} onClick={onDecrement}>
+        - 1
+      </button>
     </div>
   );
 }
