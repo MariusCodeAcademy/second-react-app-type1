0f9d87d28c4d4dd6dde50aaa11284b7b75d9b094
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-29 08:59:56 +0300

    useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
-// import MyBtn from './components/MyBtn';
+import MyBtn from './components/MyBtn';
 
 function App() {
+  // state - to kompnento busenos duomenys
+  const myArr = ['green', 'blue', 'red'];
+  console.log('App.js componentas');
   return (
     <div>
       <h2>App</h2>
+      <ul>
+        {myArr.map((c, idx) => (
+          <li key={idx}>{c}</li>
+        ))}
+      </ul>
+      <MyBtn />
     </div>
   );
 }
